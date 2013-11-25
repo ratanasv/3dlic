e92a3430c18ce13c1cbbb6b7ef25abaf668cb7a7
ratanasv <ratanasv@eecs.oregonstate.edu>
2013-11-24 21:24:38 -0800

    add the actualy implementation that will jus tthrow expcetions :)

---
+++ src/mfalk_reader_wrapper.cpp
@@ -0,0 +1,43 @@
+#include "StdAfx.h"
+#include "mfalk_reader_wrapper.h"
+
+MFalkDataTex3DFactory::MFalkDataTex3DFactory( const string& fileName ) {
+
+}
+
+
+GLenum MFalkDataTex3DFactory::getInternalFormat()
+{
+	throw std::exception("The method or operation is not implemented.");
+}
+
+int MFalkDataTex3DFactory::getWidth()
+{
+	throw std::exception("The method or operation is not implemented.");
+}
+
+int MFalkDataTex3DFactory::getHeight()
+{
+	throw std::exception("The method or operation is not implemented.");
+}
+
+int MFalkDataTex3DFactory::getDepth()
+{
+	throw std::exception("The method or operation is not implemented.");
+}
+
+GLenum MFalkDataTex3DFactory::getFormat()
+{
+	throw std::exception("The method or operation is not implemented.");
+}
+
+GLenum MFalkDataTex3DFactory::getType()
+{
+	throw std::exception("The method or operation is not implemented.");
+}
+
+void* MFalkDataTex3DFactory::get_data()
+{
+	throw std::exception("The method or operation is not implemented.");
+}
+
--- src/mfalk_reader_wrapper.h
+++ src/mfalk_reader_wrapper.h
@@ -6,3 +6,22 @@
 #pragma once
 #include "reader.h"
 #include "virtex.h"
+#include <cstring>
+
+using std::string;
+
+
+class MFalkDataTex3DFactory : public TextureAbstractFactory {
+private:
+	DatFile _datFile;
+public:
+	MFalkDataTex3DFactory(const string& fileName);
+	virtual GLenum getInternalFormat();
+	virtual int getWidth();
+	virtual int getHeight();
+	virtual int getDepth();
+	virtual GLenum getFormat();
+	virtual GLenum getType();
+	virtual void* get_data();
+
+};
